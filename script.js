0660ceff5b79a0668d866c21fd21fe5beb6b1efc
Gaurav Varia <gauravvaria93@gmail.com>
2024-06-27 12:23:05 +0530

    Input submits when enter key is pressed, Displaying temperature

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const input = document.getElementById('input');
 document.getElementById('submit').addEventListener('click', function submitData() {
-    const input = document.getElementById('input');
     const city = input.value;
     fetchWeatherData(city);
+})
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const input = document.getElementById('input');
+        const city = input.value;
+        fetchWeatherData(city);
+    }
 })
 
 async function fetchWeatherData(city) {
@@ -29,10 +37,35 @@ async function fetchWeatherData(city) {
         }
         const data = await response.json();
         console.log(data);
-        console.log(data.main.temp);
 
-        document.getElementById('city').innerHTML=`Forecast in: ${data.name}`;
+        document.getElementById('city').innerHTML = `Forecast in: ${data.name}`;
 
+        function updatetime() {
+            let timestamp = data.dt;
+
+            // Convert the Unix timestamp to milliseconds
+            const date = new Date(timestamp * 1000);
+
+            const month = date.getMonth(); // Months are zero-indexed
+            const day = date.getDate();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+
+            const dayOfWeek = date.getDay();
+            const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            const dayName = daysOfWeek[dayOfWeek];
+
+            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            const monthName = months[month];
+
+            const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+            document.getElementById('time').innerHTML = `${dayName}, ${day} ${monthName} at ${formattedTime}`;
+        }
+        updatetime();
+
+        document.getElementById('currTemp').innerHTML = data.main.temp.toFixed(1);
+        document.getElementById('high').innerHTML = `High ${data.main.temp_max.toFixed(1)}`;
+        document.getElementById('low').innerHTML = `Low ${data.main.temp_min.toFixed(1)}`;
     } catch (error) {
         console.error('Error fetching data: ', error);
     }
